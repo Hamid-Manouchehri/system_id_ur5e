ffb8c2d5e18d57fe0629354c6cfff27275b2a352
Hamid-Manouchehri <hamid.manouchehri.njf@gmail.com>
2025-05-08 14:09:28 -0400

    init '_read_write_ur5e.py' module

--- ur5e_rtde/_read_write_ur5e.py
+++ ur5e_rtde/_read_write_ur5e.py
@@ -2,13 +2,13 @@
 *******************************************************************************
 
 Project: system-id ur5e
-File: test_ur5e.py
+File: _read_write_ur5e.py
 Author: Hamid Manouchehri
 Email: hmanouch@buffalo.edu
 Date: May 8, 2025
 
 Description:
-Python script using RTDE protocol for reading status of UR5e.
+Python module using RTDE protocol for reading and writing commands of UR5e.
 
 License:
 This script is licensed under the MIT License.
@@ -27,31 +27,38 @@ with the software or the use or other dealings in the software.
 
 *******************************************************************************
 """
-#!/usr/bin/env python3
 
 import numpy as np
-import rtde_receive
 import rtde_control
+import rtde_receive
 import yaml
-import ur5e_rtde._read_write_ur5e as ur5e
+
 
 with open("../config/config.yml", 'r') as file:
     config = yaml.safe_load(file)
 
 ROBOT_ID = config["UR5E"]["ROBOT_ID"]
 
+# Initialize RTDE interfaces
+rtde_receive_interface = rtde_receive.RTDEReceiveInterface(ROBOT_ID)
+rtde_control_interface = rtde_control.RTDEControlInterface(ROBOT_ID)
 
-if __name__ == "__main__":
-
-    try:
-        
-        ur5e.ur5e_homming()
-
-        # print(ur5e.get_tcp_pose())
-        # ur5e.ur5e_moveJ([1.74382, -0.30455, 0.41068, -0.08044, 1.0248, 3.16844], speed=0.5, accel=0.2)  # TODO
-        
-        # ur5e.ur5e_moveL(tcp_position, speed=speed, acceleration=accel, asynchronous=False)
+def get_tcp_pose():
+    rtde = rtde_receive.RTDEReceiveInterface(ROBOT_ID)
+    pose = rtde.getActualTCPPose()
+    rtde.disconnect()
+    return pose
 
 
-    except Exception as e:
-        print(f"An error occurred: {e}")
+def ur5e_moveJ(joint_goal, speed=0.1, accel=0.5):
+    rtde_control_interface.moveJ(np.array(joint_goal), speed=speed, acceleration=accel)
+
+
+# def ur5e_moveL():
+#     tcp_position = np.array([-0.12433, -0.67481, 0.17184, -0.89113, 1.6404, -1.65547])  # m and Rad
+#     rtde_control_interface.moveL(tcp_position, speed=speed, acceleration=accel, asynchronous=False)
+
+def ur5e_homming(speed=0.1, accel=0.5):
+    """ UR5e upright."""
+    joint_home_position = np.array([0.0, -1.57, 0.0, -1.57, 0.0, 0.0])  # Rad
+    rtde_control_interface.moveJ(joint_home_position, speed=speed, acceleration=accel)
